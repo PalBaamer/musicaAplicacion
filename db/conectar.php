949b7829633957f1f78413a4ce679a0e1d9db402
PalBaamer <47423242+PalBaamer@users.noreply.github.com>
2019-02-18 12:49:08 +0100

    NuevosArchivos

---
+++ db/conectar.php
@@ -0,0 +1,16 @@
+<?php
+echo "Iniciando conexion BD"."<br>";
+
+// Creación de una clase que nos permita conectarnos a la BD
+class Conectar{
+    public static function conexion(){
+		//10.128.98.161
+        $conexion=new mysqli("10.128.98.161", "root", "rootroot", "musica");
+		if($conexion!=null){
+	    return $conexion;
+		}else{
+			die("Error conexión: " . mysqli_connect_error());
+		}
+	}
+}
+?>
