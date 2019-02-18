756ad5988ef14a395d052413ab39f5506b8287db
PalBaamer <47423242+PalBaamer@users.noreply.github.com>
2019-02-18 17:32:46 +0100

    Ip

--- db/conectar.php
+++ db/conectar.php
@@ -4,8 +4,7 @@ echo "Iniciando conexion BD"."<br>";
 // Creación de una clase que nos permita conectarnos a la BD
 class Conectar{
     public static function conexion(){
-		//10.128.98.161
-        $conexion=new mysqli("10.129.14.174", "root", "rootroot", "musica");
+        $conexion=new mysqli("10.129.14.208", "root", "rootroot", "musica");
 		if($conexion!=null){
 	    return $conexion;
 		}else{
